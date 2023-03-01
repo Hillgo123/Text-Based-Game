3701a0ac5eb820ba2cc5c783096ab1f69ef104b8
Leo Bergqvist <leo@bergqvist.se>
2023-03-01 21:44:42 +0100

    colors + clear console +

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import sys
+import os
 from termcolor import colored
 
 
-class initial_opt:
-    def __init__(self, name, event_color, actions_color):
+class store_options:
+    """Store options."""
+
+    def __init__(self, name: str, event_color: str, actions_color: str):
         self.name = name
         self.event_color = event_color
         self.actions_color = actions_color
+        self.all_colors = ['light_grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white',
+                           'light_red', 'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan']
+
+
+class store_responses:
+    """Store responses and handle consequences."""
+
+    def __init__(self):
+        pass
 
 
 def check_response(response: str, possible_responses: list):
     """Checks if the response is valid."""
 
     while True:
-        response = input(colored('>', opt.actions_color))
+        response = input(colored('> ', opt.actions_color))
         try:
             response = int(response)
 
@@ -23,6 +35,10 @@ def check_response(response: str, possible_responses: list):
         if response == 'q':
             print(colored('Quitting...', opt.actions_color))
             sys.exit()
+
+        if response == 'h':
+            print(colored('Help is not available yet.', opt.event_color))
+            continue
 
         if response in possible_responses:
             return response
@@ -34,49 +50,90 @@ def check_response(response: str, possible_responses: list):
 def print_actions(actions: list, possible_responses: list):
     """Prints the actions."""
 
-    for idn, n in enumerate(actions):
-        print(colored(f'{idn + 1}:  {n}', opt.actions_color))
-        possible_responses.append(idn + 1)
+    for id_action, action in enumerate(actions):
+        print(colored(f'{id_action + 1}:  {action}', opt.actions_color))
+        possible_responses.append(id_action + 1)
+
+
+def clear_console():
+    """Clears the console."""
+
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def start():
+    """Start menu."""
+
     start_actions = [f'Play as {opt.name}', 'Options', 'Help', 'Quit']
     p_r = []
-    start_r = None
+    r = None
     print(colored('Welcome to the game!', opt.event_color))
 
     print_actions(start_actions, p_r)
-    start_r = check_response(start_r, p_r)
+    r = check_response(r, p_r)
 
-    if start_r == 1:
+    if r == 1:
+        clear_console()
         event_1()
 
-    elif start_r == 2:
+    elif r == 2:
+        clear_console()
         options()
 
-    elif start_r == 3:
+    elif r == 3:
+        clear_console()
         print(colored('Help is not available yet.', opt.event_color))
         start()
 
-    elif start_r == 4:
+    elif r == 4:
         print(colored('Quitting...', opt.actions_color))
         sys.exit()
 
 
 def options():
+    """Options menu."""
+
     opt_actions = [f'Name: {opt.name}', f'Event Color: {opt.event_color}',
                    f'Actions Color: {opt.actions_color}', 'Back']
     p_r = []
-    opt_r = None
+    r = None
 
     print_actions(opt_actions, p_r)
-    opt_r = check_response(opt_r, p_r)
+    r = check_response(r, p_r)
 
-    if opt_r == 1:
+    if r == 1:
         opt.name = input(colored('New name > ', opt.actions_color))
+        clear_console()
         options()
 
-    elif opt_r == 4:
+    elif r == 2:
+        print(
+            colored('Colors: ', opt.event_color) + colored(f'{", ".join(opt.all_colors)}', opt.actions_color))
+
+        new_color = ''
+        print(
+            colored('New event color:', opt.event_color))
+
+        opt.event_color = check_response(new_color, opt.all_colors)
+
+        clear_console()
+        options()
+
+    elif r == 3:
+        print(
+            colored('Colors: ', opt.event_color) + colored(f'{", ".join(opt.all_colors)}', opt.actions_color))
+
+        new_color = ''
+        print(
+            colored('New actions color:', opt.event_color))
+
+        opt.actions_color = check_response(new_color, opt.all_colors)
+
+        clear_console()
+        options()
+
+    elif r == 4:
+        clear_console()
         start()
 
 
@@ -84,7 +141,9 @@ def event_1():
     pass
 
 
-opt = initial_opt('Player', 'white', 'green')
+opt = store_options('Player', 'white', 'green')
+responses = store_responses()
 
 if __name__ == '__main__':
+    clear_console()
     start()
